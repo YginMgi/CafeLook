df308e40059db1319cc8981e2db99ca1abbe3b83
Y00ujin <yujingim43@gmail.com>
2022-03-11 15:20:32 +0900

    [CREATE] cafeCollectionView created

--- CafeLook/CafeCollectionViewCell.swift
+++ CafeLook/CafeCollectionViewCell.swift
@@ -2,11 +2,75 @@
 //  CafeCollectionViewCell.swift
 //  CafeLook
 //
-//  Created by 김민지 on 2022/03/10.
+//  Created by 김유진 on 2022/03/11.
 //
 
 import UIKit
 
 class CafeCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var cafeImage: UIImageView!
+    static let identifier = "\(CafeCollectionViewCell.self)"
+    
+    private var bgView: UIView = {
+       let v = UIView()
+        v.backgroundColor = .white
+        v.layer.cornerRadius = 13
+        v.layer.borderWidth = 2
+        v.layer.borderColor = UIColor(red: 229/255, green: 223/255, blue: 221/255, alpha: 1).cgColor
+        return v
+    }()
+    
+    private lazy var cafeLabelView: UIView = {
+        let v = UIView()
+        v.addSubview(cafeName)
+        v.addSubview(cafeLocation)
+        v.backgroundColor = .green
+        return v
+    }()
+    
+    private var cafeName: UILabel = {
+        let l = UILabel()
+        l.font = UIFont(name: "ARLRDBD", size: 20)
+        l.textColor = UIColor(red: 118/255, green: 96/255, blue: 87/255, alpha: 1)
+        return l
+    }()
+    
+    private var locationImage = UIImage()
+    private var cafeLocation = UILabel()
+    private var openTitle = UILabel()
+    private var closeTitle = UILabel()
+    private var openTime = UILabel()
+    private var closeTime = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView(){
+        contentView.addSubview(bgView)
+        bgView.addSubview(cafeLabelView)
+        
+        bgView.translatesAutoresizingMaskIntoConstraints = false
+        cafeLabelView.translatesAutoresizingMaskIntoConstraints = false
+        
+        bgView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bgView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        bgView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        bgView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        
+        cafeLabelView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        cafeLabelView.heightAnchor.constraint(equalToConstant: self.frame.height/2).isActive = true
+        cafeLabelView.widthAnchor.constraint(equalToConstant: self.frame.width/2).isActive = true
+        cafeLabelView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: contentView.frame.width/11.03).isActive = true
+        
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 5
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        contentView.layer.masksToBounds = true
+    }
 }
--- CafeLook/ViewController.swift
+++ CafeLook/ViewController.swift
@@ -8,9 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var gwangjuBtn: UIButton!
+    
+    private let cafeCollectionView: UICollectionView = {
+         let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+         return cv
+     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
     }
+    
+    private func setupView(){
+        cafeCollectionView.delegate = self
+        cafeCollectionView.dataSource = self
+        
+        cafeCollectionView.register(CafeCollectionViewCell.self, forCellWithReuseIdentifier: CafeCollectionViewCell.identifier)
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(cafeCollectionView)
+        
+        cafeCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        cafeCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        cafeCollectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        cafeCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        cafeCollectionView.topAnchor.constraint(equalTo: gwangjuBtn.bottomAnchor, constant: view.frame.height/13.7).isActive = true
+    }
+}
 
 
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CafeCollectionViewCell.identifier, for: indexPath) as? CafeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout{
+    //MARK: collectionview - cell height
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize.init(width: self.view.frame.width/1.25, height: self.view.frame.height/10)
+    }
+     
+    //MARK: collectionView - left Padding
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: self.view.frame.width/9.61, bottom: 0, right: self.view.frame.width/9.61)
+    }
+    
+    //MARK: collectionView - cell 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return view.frame.height/62.4
+    }
 }
